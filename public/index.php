d729e2d80c2d27106b10fc45b9cf9af9f26f88d5
RobertFeketeDev <fekete.robert.dev@gmail.com>
2026-07-22 13:23:41 +0200

    Other changes

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Core\Router;
-use App\Core\Controller;
+use App\Controllers\HomeController;
 
 $router = new Router();
 
